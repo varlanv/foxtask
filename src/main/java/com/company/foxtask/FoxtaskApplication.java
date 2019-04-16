1afb61c0d92243a25874e894737a274866714bdf
varlanv <varlanv96@gmail.com>
2019-04-16 18:30:16 +0300

    first commit

---
+++ src/main/java/com/company/foxtask/FoxtaskApplication.java
@@ -0,0 +1,13 @@
+package com.company.foxtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoxtaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoxtaskApplication.class, args);
+    }
+
+}
